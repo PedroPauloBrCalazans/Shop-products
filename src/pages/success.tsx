67085f0a868d467a0005d7524c64c8bf165a4f87
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2023-08-29 16:55:30 -0300

    feat: Estrutura da página de sucesso.

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
   return (
-    <div>
-      <h1>Success</h1>
-    </div>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Pedro Paulo</strong>, sua <strong>Camiseta VW ar</strong>{" "}
+        já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
   );
 }
